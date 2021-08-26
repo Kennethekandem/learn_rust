3d0c39e4c3bc9df0e393e421e2fb0694cfa666c0
Kenneth Ekandem <kennyekandem@gmail.com>
2021-08-26 12:33:15 +0100

    first push

---
+++ target/debug/deps/learn_rust.d
@@ -0,0 +1,5 @@
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs
+
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs
+
+src/main.rs:
