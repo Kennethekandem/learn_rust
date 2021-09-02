852dd4e62942e49f0e834dc25eafc6d675a794b1
Kenneth Ekandem <kennyekandem@gmail.com>
2021-09-02 20:42:29 +0100

    update: types

--- target/debug/deps/learn_rust.d
+++ target/debug/deps/learn_rust.d
@@ -1,7 +1,8 @@
-/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs src/print.rs src/var.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs src/print.rs src/var.rs src/types.rs
 
-/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs src/print.rs src/var.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs src/print.rs src/var.rs src/types.rs
 
 src/main.rs:
 src/print.rs:
 src/var.rs:
+src/types.rs:
