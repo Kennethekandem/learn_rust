7daccff43e207e43049058f3eeda67f9bd8c4aba
Kenneth Ekandem <kennyekandem@gmail.com>
2021-09-02 21:10:15 +0100

    update: strings

--- target/debug/deps/learn_rust.d
+++ target/debug/deps/learn_rust.d
@@ -1,8 +1,6 @@
-/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs src/print.rs src/var.rs src/types.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs src/strings.rs
 
-/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs src/print.rs src/var.rs src/types.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs src/strings.rs
 
 src/main.rs:
-src/print.rs:
-src/var.rs:
-src/types.rs:
+src/strings.rs:
