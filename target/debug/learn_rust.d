301a354c82f68acaa5dd67851726e9732ab5c3bf
Kenneth Ekandem <kennyekandem@gmail.com>
2021-10-06 00:09:35 +0100

    functions

--- target/debug/learn_rust.d
+++ target/debug/learn_rust.d
@@ -1,1 +1,1 @@
-/Users/m1pro/Documents/projects/learn_rust/target/debug/learn_rust: /Users/m1pro/Documents/projects/learn_rust/src/loops.rs /Users/m1pro/Documents/projects/learn_rust/src/main.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/learn_rust: /Users/m1pro/Documents/projects/learn_rust/src/functions.rs /Users/m1pro/Documents/projects/learn_rust/src/main.rs
