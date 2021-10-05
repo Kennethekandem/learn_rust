0fab39e9fbacf624c853e374032e7df21320dd80
Kenneth Ekandem <kennyekandem@gmail.com>
2021-10-05 23:39:14 +0100

    vectors

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod var;
 mod types;
 mod strings;
 mod tuples;*/
-mod arrays;
+// mod arrays;
+mod vectors;
 fn main() {
     /*print::run();
     var::run();
     types::run();
     strings::run();
     tuples::run();*/
-    arrays::run()
+    // arrays::run();
+    vectors::run()
 }
--- target/debug/learn_rust.d
+++ target/debug/learn_rust.d
@@ -1,1 +1,1 @@
-/Users/m1pro/Documents/projects/learn_rust/target/debug/learn_rust: /Users/m1pro/Documents/projects/learn_rust/src/arrays.rs /Users/m1pro/Documents/projects/learn_rust/src/main.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/learn_rust: /Users/m1pro/Documents/projects/learn_rust/src/main.rs /Users/m1pro/Documents/projects/learn_rust/src/vectors.rs
