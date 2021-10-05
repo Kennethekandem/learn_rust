fac321f7dc9d86e5eb12602f09ce7c4b8dcb2cd0
Kenneth Ekandem <kennyekandem@gmail.com>
2021-10-05 23:17:16 +0100

    tuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -4,4 +4,6 @@ Max is 12 elements
 
 pub fn run() {
     let person: (&str, &str, i8) = ("kenneth", "Mass", 30);
+
+    print!("{} is from {} and is {}", person.0, person.1, person.2)
 }
--- target/debug/deps/learn_rust.d
+++ target/debug/deps/learn_rust.d
@@ -1,6 +1,6 @@
-/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs src/strings.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs src/tuples.rs
 
-/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs src/strings.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs src/tuples.rs
 
 src/main.rs:
-src/strings.rs:
+src/tuples.rs:
