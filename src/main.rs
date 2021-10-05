ee79108e897a99cc1e9869a2fd42f4f391d77fbe
Kenneth Ekandem <kennyekandem@gmail.com>
2021-10-06 00:01:19 +0100

    loops

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
-mod conditionals;
+mod loops;
 
 fn main() {
-    conditionals::run();
+    loops::run();
 }
--- target/debug/deps/learn_rust.d
+++ target/debug/deps/learn_rust.d
@@ -1,6 +1,6 @@
-/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs src/conditionals.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust: src/main.rs src/loops.rs
 
-/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs src/conditionals.rs
+/Users/m1pro/Documents/projects/learn_rust/target/debug/deps/learn_rust.d: src/main.rs src/loops.rs
 
 src/main.rs:
-src/conditionals.rs:
+src/loops.rs:
